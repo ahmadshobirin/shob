eddae7932c2eb2c428332ec1005f50790a2b0fd5
ahmadshobirin <ahmadshobirin.dev@gmail.com>
2020-10-24 01:36:21 +0700

    get user by token, get token or refresh token

--- routes/api.php
+++ routes/api.php
@@ -2,5 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::post('login', [\App\Http\Controllers\Api\AuthController::class, 'login'])->name('login');
+Route::group(["prefix" => "v1"], function(){
+
+    //Auth
+    Route::group(["prefix" => "auth"], function(){
+        Route::post("login", [\App\Http\Controllers\Api\AuthController::class, 'login'])->name('login');
+        Route::get("token", [\App\Http\Controllers\Api\AuthController::class, "GetTokenOrRefreshToken"])->name("token");
+    });
+
+    //Profile
+    Route::group(["prefix" => "profile", "middleware" => "jwt.verify"], function(){
+        Route::get("", [\App\Http\Controllers\Api\AuthController::class, 'AuthenticatedUser'])->name('authenticated');
+    });
+
+});
 
